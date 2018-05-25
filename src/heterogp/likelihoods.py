2c503eebb1d43bbf513fb9e45661f0b563ac2d92
joshuaalbert <albert@strw.leidenuniv.nl>
2018-05-25 14:24:53 +0200

    remove old code

--- src/heterogp/likelihoods.py
+++ src/heterogp/likelihoods.py
@@ -92,27 +92,6 @@ class HeteroscedasticLikelihood(Likelihood):
                 Fmu, Fvar, hetero_variance=hetero_variance, **args)
         V_y = E_y2 - tf.square(E_y)
         return E_y, V_y
-
-        gh_x, gh_w = hermgauss(self.num_gauss_hermite_points)
-        gh_w /= np.sqrt(np.pi)
-        gh_w = gh_w.reshape(-1, 1)
-        shape = tf.shape(Fmu)
-        Fmu, Fvar,hetero_variance = [tf.reshape(e, (-1, 1)) for e in (Fmu, Fvar,hetero_variance)]
-        # each element of arg must have same number of latent
-        # assumes heteroscedastic error has same shape as Y
-        # TODO  tile to match latent if not already
-        args = [tf.reshape(e, (-1, 1)) if isinstance(e,tf.Tensor) else e for e in args]
-        X = gh_x[None, :] * tf.sqrt(2.0 * Fvar) + Fmu
-
-        # here's the quadrature for the mean
-        conditional_mean = self.conditional_mean(X,hetero_variance, *args)
-        E_y = tf.reshape(tf.matmul(conditional_mean, gh_w), shape)
-
-        # here's the quadrature for the variance
-        integrand = self.conditional_variance(X, hetero_variance, *args)             + tf.square(conditional_mean)
-        V_y = tf.reshape(tf.matmul(integrand, gh_w), shape) - tf.square(E_y)
-
-        return E_y, V_y
     
     def predict_density(self, Fmu, Fvar, Y, hetero_variance=None, **args):
         """
@@ -132,19 +111,6 @@ class HeteroscedasticLikelihood(Likelihood):
                 Fmu, Fvar, Y=Y, hetero_variance=hetero_variance, **args)
         return tf.log(exp_p)
 
-        gh_x, gh_w = hermgauss(self.num_gauss_hermite_points)
-
-        gh_w = gh_w.reshape(-1, 1) / np.sqrt(np.pi)
-        shape = tf.shape(Fmu)
-        Fmu, Fvar, Y,hetero_variance = [tf.reshape(e, (-1, 1)) for e in (Fmu, Fvar, Y,hetero_variance)]
-        args = [tf.reshape(e, (-1, 1)) if isinstance(e,tf.Tensor) else e for e in args]
-        X = gh_x[None, :] * tf.sqrt(2.0 * Fvar) + Fmu
-
-        Y = tf.tile(Y, [1, self.num_gauss_hermite_points])  # broadcast Y to match X
-
-        logp = self.logp(X, Y, hetero_variance, *args)
-        return tf.reshape(tf.log(tf.matmul(tf.exp(logp), gh_w)), shape)
-
     @params_as_tensors
     def variational_expectations(self, Fmu, Fvar, Y, hetero_variance=None, **args):
         """
@@ -162,17 +128,7 @@ class HeteroscedasticLikelihood(Likelihood):
         return ndiagquad(self.logp,
                 self.num_gauss_hermite_points,
                 Fmu, Fvar, Y=Y, hetero_variance=hetero_variance, **args)
-        gh_x, gh_w = hermgauss(self.num_gauss_hermite_points)
-        gh_x = gh_x.reshape(1, -1)
-        gh_w = gh_w.reshape(-1, 1) / np.sqrt(np.pi)
-        shape = tf.shape(Fmu)
-        Fmu, Fvar, Y,hetero_variance = [tf.reshape(e, (-1, 1)) for e in (Fmu, Fvar, Y, hetero_variance)]
-        args = [tf.reshape(e, (-1, 1)) if isinstance(e,tf.Tensor) else e for e in args]
-        X = gh_x * tf.sqrt(2.0 * Fvar) + Fmu
-        Y = tf.tile(Y, [1, self.num_gauss_hermite_points])  # broadcast Y to match X
-        logp = self.logp(X, Y, hetero_variance, *args)
-        return tf.reshape(tf.matmul(logp, gh_w), shape)
-
+        
 class HeteroscedasticGaussian(HeteroscedasticLikelihood):
     def __init__(self, log_noise_latent, name=None):
         super().__init__(log_noise_latent, name=name)
